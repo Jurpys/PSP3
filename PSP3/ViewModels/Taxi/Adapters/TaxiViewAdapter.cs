742473a97b72e6765a5065f6c64bcafcec3266e0
Jurpys <jurgaitis.mantas@gmail.com>
2015-12-17 10:45:24 +0200

    Added adapter

---
+++ PSP3/ViewModels/Taxi/Adapters/TaxiViewAdapter.cs
@@ -0,0 +1,12 @@
+﻿using PSP3.Domain;
+
+namespace PSP3.ViewModels
+{
+    public class TaxiViewAdapter
+    {
+        public string GetOrderId(ObservableTaxi taxi)
+        {
+            return taxi.IsBusy? taxi.OrderId.ToString() : "None";
+        } 
+    }
+}
--- PSP3/ViewModels/Taxi/SimpleTaxiView.cs
+++ PSP3/ViewModels/Taxi/SimpleTaxiView.cs
@@ -7,6 +7,11 @@ namespace PSP3.ViewModels
     public class SimpleTaxiView : TaxiView
     {
         private string DriverName;
+        private int CurrentLocation;
+        private double Tariff;
+        private string OrderId;
+
+        private TaxiViewAdapter adapter = new TaxiViewAdapter();
 
         ObservableTaxiRepository _taxiRepository;
         public SimpleTaxiView(int id, ObservableTaxiRepository taxiRepository) : base(id)
@@ -19,11 +24,16 @@ namespace PSP3.ViewModels
         {
             Console.WriteLine("Taxi {0}:", _id);
             Console.WriteLine("Driver's name: {0}", DriverName);
+            Console.WriteLine($"Tariff: {Tariff}");
+            Console.WriteLine($"OrderId: {OrderId}");
         }
 
         protected override void Update(ObservableTaxi taxi)
         {
             DriverName = taxi.DriverName;
+            CurrentLocation = taxi.CurrentLocation;
+            Tariff = taxi.Tariff;
+            OrderId = adapter.GetOrderId(taxi);
         }
 
         private void Initialize()
